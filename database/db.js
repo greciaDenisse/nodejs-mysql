060cce22ac1dd9bf857feff724fbfe12848a4375
greciaDenisse <greciaabarcaarroyo24@gmail.com>
2022-11-08 01:18:15 -0600

    BD

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv"
 
 dotenv.config()
 
-const db = new Sequelize (DB_NAME||process.env.DB_DATABASE,DB_USER||process.env.DB_USER,DB_PASSWORD||process.env.DB_PASSWORD,{
+const db = new Sequelize (DB_NAME||process.env.DB_NAME,DB_USER||process.env.DB_USER,DB_PASSWORD||process.env.DB_PASSWORD,{
     host:DB_HOST||process.env.DB_HOST,
     port:DB_PORT||process.env.DB_PORT,
     dialect:'mysql'||process.env.DB_DIALECT
