1721d19b4c35a81d380cb50a394ab754c11d1e30
greciaDenisse <greciaabarcaarroyo24@gmail.com>
2022-11-07 23:32:17 -0600

    comman añadido

--- database/db.js
+++ database/db.js
@@ -4,6 +4,7 @@ import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from "../config.js";
 
 const db = new Sequelize (DB_NAME,DB_USER,DB_PASSWORD,{
     DB_HOST,
+    DB_PORT,
     dialect:'mysql'
 })
 
