bbab36dcb8c3e01dc2eb806c1834d62de2ee8a84
greciaDenisse <greciaabarcaarroyo24@gmail.com>
2022-11-08 01:21:37 -0600

    db1

--- database/db.js
+++ database/db.js
@@ -3,10 +3,10 @@ import * as dotenv from "dotenv"
 
 dotenv.config()
 
-const db = new Sequelize (DB_NAME||process.env.DB_NAME,DB_USER||process.env.DB_USER,DB_PASSWORD||process.env.DB_PASSWORD,{
-    host:DB_HOST||process.env.DB_HOST,
-    port:DB_PORT||process.env.DB_PORT,
-    dialect:'mysql'||process.env.DB_DIALECT
+const db = new Sequelize (process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    dialect:process.env.DB_DIALECT
 })
 
 export default db
