55ee6375c3dbad37c89f8c88801ef3a9cd3c1ac6
Santiago <moralezz2589@gmail.com>
2022-11-14 20:49:04 -0600

    con bod

--- controllers/ControllerBodega.js
+++ controllers/ControllerBodega.js
@@ -1,9 +1,14 @@
 import { Bodegas } from "../models/Models.js"
+import { Sequelize } from "sequelize";
+import db from "../database/db.js";
 
 
 export const getAllBodegas = async (req,res) => {
     try{
-        const bodegas= await Bodegas.findAll()
+        const bodegas = await db.query(
+            'SELECT * FROM bodegas_mat where estadoBod = 1',
+            {type: db.QueryTypes.SELECT}
+        )
         res.json(bodegas)
    }catch(error){
            res.json({message:error.message})
